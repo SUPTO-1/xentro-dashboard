9d3b076360ac3baf16dc270721a4f016381f9418
SUPTO-1 <jahidulislamsupta9@gmail.com>
2025-02-07 02:59:14 +0600

    fetching data from user api

--- src/Dashboard/AllUsers.jsx
+++ src/Dashboard/AllUsers.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import { data } from "react-router-dom";
+
 const AllUsers = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(data=>{
+            setUsers(data)
+            setLoading(false)
+        })
+    })
+    console.log(users);
+    if(loading){
+        return <progress className="progress w-56 flex justify-center items-center mx-auto mt-16"></progress>
+    }
     return (
         <div>
             
