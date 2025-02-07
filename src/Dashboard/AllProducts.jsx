2c7d220c58e283195bab5d804f22df2973e3298b
SUPTO-1 <jahidulislamsupta9@gmail.com>
2025-02-07 19:04:32 +0600

    search and sorting implemented

--- src/Dashboard/AllProducts.jsx
+++ src/Dashboard/AllProducts.jsx
@@ -3,46 +3,84 @@ import SingleProduct from "./SingleProduct";
 import Swal from "sweetalert2";
 
 const AllProducts = () => {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(()=>{
-        fetch("https://api.restful-api.dev/objects")
-        .then(res => res.json())
-        .then(data => {
-           setProducts(data);
-            setLoading(false);
-        })
-    },[]);
-    const handleDelete = (id) => {
-        fetch(`https://api.restful-api.dev/objects/${id}`, {
-            method: "DELETE",
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                const remaining = products.filter((product) => product.id !== id);
-                setProducts(remaining);
-                Swal.fire("Deleted!", "Your product has been deleted.", "success");
-            });
-    }
-    //console.log(products);
-    if(loading){
-        return (
-            <div className="flex justify-center items-center h-screen">
-            <progress className="progress w-56"></progress>
-          </div>
-        )
-    }
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [sort,setSort] = useState('');
+  useEffect(() => {
+    fetch("https://api.restful-api.dev/objects")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setLoading(false);
+      });
+  }, []);
+  const handleDelete = (id) => {
+    fetch(`https://api.restful-api.dev/objects/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const remaining = products.filter((product) => product.id !== id);
+        setProducts(remaining);
+        Swal.fire("Deleted!", "Your product has been deleted.", "success");
+      });
+  };
+  const handleSort = (type) => {
+    setSort(type);
+  }
+  const sortedProducts = products.sort((a, b) => {
+    const priceA = a.data?.price || 0;
+    const priceB = b.data?.price || 0;
+    if (sort === "asc") {
+        return priceA - priceB;
+      } else if (sort === "desc") {
+        return priceB - priceA;
+      }
+  })
+  //console.log(products);
+  if (loading) {
     return (
-        <div>
-            <h2 className="text-3xl font-poppins font-semibold text-center mt-10">Total Products : {products.length}</h2>
-            <div className="divider mb-5"></div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center">
-                {
-                    products.map(product => <SingleProduct key={product.id} product={product} handleDelete={handleDelete}></SingleProduct>)
-                }
-            </div>
-        </div>
+      <div className="flex justify-center items-center h-screen">
+        <progress className="progress w-56"></progress>
+      </div>
     );
+  }
+  return (
+    <div>
+     <div className="md:flex justify-between p-4">
+      <form className="filter mt-10">
+        <input className="btn btn-square" type="reset" value="×" />
+        <input
+          className="btn"
+          type="radio"
+          name="sort"
+          aria-label="ascending"
+          onChange={()=>handleSort("asc")}
+        />
+        <input
+          className="btn"
+          type="radio"
+          name="sort"
+          aria-label="descending"
+          onChange={()=>handleSort("desc")}
+        />
+      </form>
+      <h2 className="text-3xl font-poppins font-semibold text-center mt-10">
+        Total Products : {products.length}
+      </h2>
+     </div>
+      <div className="divider mb-5"></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center">
+        {sortedProducts.map((product) => (
+          <SingleProduct
+            key={product.id}
+            product={product}
+            handleDelete={handleDelete}
+          ></SingleProduct>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllProducts;
--- src/Dashboard/AllUsers.jsx
+++ src/Dashboard/AllUsers.jsx
@@ -4,6 +4,7 @@ import SingleUser from "./SingleUser";
 const AllUsers = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
@@ -11,24 +12,48 @@ const AllUsers = () => {
         setUsers(data);
         setLoading(false);
       });
-  },[users]);
+  }, [users]);
   //console.log(users);
+  const searchUser = users.filter((user) => {
+    return user.name.toLowerCase().includes(search.toLowerCase());
+  })
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen">
-      <progress className="progress w-56"></progress>
-    </div>
+        <progress className="progress w-56"></progress>
+      </div>
     );
   }
   return (
     <div className="mt-4">
       <div className="text-center mt-10">
-        <h2 className="text-3xl font-poppins font-bold text-[#213555]">
-          Total User : {users.length}
-        </h2>
+        <div className="md:flex justify-between p-4">
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search" onChange={(e)=>setSearch(e.target.value)} />
+          </label>
+          <h2 className="text-3xl font-poppins font-bold text-[#213555]">
+            Total User : {users.length}
+          </h2>
+        </div>
         <div className="divider mb-10"></div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center">
-          {users.map((user) => (
+          {searchUser.map((user) => (
             <SingleUser key={user.id} user={user}></SingleUser>
           ))}
         </div>
