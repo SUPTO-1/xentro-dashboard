fbc1a13c18e1997f69994838d684d65a834b21f9
SUPTO-1 <jahidulislamsupta9@gmail.com>
2025-02-07 19:41:10 +0600

    fix minor issue

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Navigate to="/dashboard" replace />
             <Outlet></Outlet>
         </div>
     );
